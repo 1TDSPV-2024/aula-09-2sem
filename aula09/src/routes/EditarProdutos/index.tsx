f6b1a3b4b062e99ae2eb913d865d5b07bc552fc7
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-06 22:39:04 -0300

    Adicionando um modal ao EditarProduto.

--- aula09/src/routes/EditarProdutos/index.tsx
+++ aula09/src/routes/EditarProdutos/index.tsx
@@ -1,40 +1,44 @@
 import { useParams } from "react-router-dom";
 import "../../listaProdutos";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ObjetoLista } from "../../types";
+import styles from "../../css/ModalEditar.module.css";
 
 export default function EditarProdutos() {
 
   //MUDANDO O TÍTULO DA PÁGINA!!!
   document.title = "EDITAR PRODUTOS";
-
-  //Realizando um destructuring para recuperar o parâmetro que foi passado na rota:
-  //Ex: Se a rota criada foi: /meus-dados/:dados
-  //O destructuring irá recuperar o dado que foi passado na rota e atribuir a propriedade de um elemento cuja o nome é aquele criado na rota, antes do dois pontos,ou seja, (:dados)
-  //Então teriamos que realizar a seguinte ação para receber esta informação.
-  // const{dados} = useParams(), um detalhe aqui é que o useParams() pertence ao react-router e deve ser importado dele
-  const { id } = useParams();
   
+  const { id } = useParams();
+
   const listaProdutosString = localStorage.getItem("lista") || '[]';
+  
   const lista:ObjetoLista[] = JSON.parse(listaProdutosString);
   
   const [produto, setProduto] = useState<ObjetoLista>();
   
+  const [isModal, setIsModal] = useState(true);
+  
+  //Utilizando o hook useRef
+  const currentDialog = useRef<HTMLDialogElement>(null);
+
   useEffect(() => {
        setProduto(lista.find((prod) => prod.id == Number(id)));
-  }, [id,lista]);
+        if(isModal){
+          currentDialog.current?.showModal();
+        }else{
+          currentDialog.current?.close();
+        }
+  }, [isModal]);
 
   return (
     <div>
-      <h1>PRODUTO A SER EDITADO</h1>
-      <div>
-        <h2>ID: {id}</h2>
-        <div>
-          <p>Nome: {produto?.nome}</p>
-          <p>Preço: {produto?.preco}</p>
-          <p>Quantidade: {produto?.qtd}</p>
-        </div>
-      </div>
+      <dialog  ref={currentDialog} className={styles.modalEditar}>
+        <h1>Editar Produtos</h1>
+          <div>
+            
+          </div>
+      </dialog>
     </div>
   );
 }
