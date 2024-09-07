58408936e04ea1fe9290aeb69eac3add6e47a556
cadupacheco <caducoelho.pacheco1303@gmail.com>
2024-09-06 23:10:58 -0300

    acabando o editar produto

--- aula09/src/routes/EditarProdutos/index.tsx
+++ aula09/src/routes/EditarProdutos/index.tsx
@@ -1,43 +1,50 @@
-import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../../listaProdutos";
-import { useEffect, useRef, useState } from "react";
+import { MinhaTabela } from "../../style/styled";
 import { ObjetoLista } from "../../types";
-import styles from "../../css/ModalEditar.module.css";
+import { useState } from "react";
 
-export default function EditarProdutos() {
+export default function Produtos() {
 
   //MUDANDO O TÍTULO DA PÁGINA!!!
-  document.title = "EDITAR PRODUTOS";
+  document.title = "PRODUTOS";
   
-  const { id } = useParams();
-
   const listaProdutosString = localStorage.getItem("lista") || '[]';
+  const lista = JSON.parse(listaProdutosString);
   
-  const lista:ObjetoLista[] = JSON.parse(listaProdutosString);
-  
-  const [produto, setProduto] = useState<ObjetoLista>();
-  
-  const [isModal, setIsModal] = useState(true);
-  
-  //Utilizando o hook useRef
-  const currentDialog = useRef<HTMLDialogElement>(null);
-
-  useEffect(() => {
-       setProduto(lista.find((prod) => prod.id == Number(id)));
-        if(isModal){
-          currentDialog.current?.showModal();
-        }else{
-          currentDialog.current?.close();
-        }
-  }, [isModal]);
+  const [produtos] = useState<ObjetoLista[]>(lista);
 
   return (
     <div>
-      <dialog  ref={currentDialog} className={styles.modalEditar}>
-        <h1>Editar Produtos</h1>
-          <div>
-          </div>
-      </dialog>
+      <h1>Produtos Eletrônicos</h1>
+      <MinhaTabela>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Quantidade</th>
+            <th>Preço</th>
+            <th>Editar / Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* COMENTÁRIO */}
+          {produtos.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.nome}</td>
+              <td>{produto.qtd}</td>
+              <td>{produto.preco}</td>
+              <td><Link to={`/editar/produtos/${produto.id}`}>Editar</Link> </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={4}>
+              Total de produtos : <span>{produtos.length}</span>
+            </td>
+          </tr>
+        </tfoot>
+      </MinhaTabela>
     </div>
   );
 }
